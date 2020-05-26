93f950caa9892651c22bd2648607a28c4789fce6
姜广庆 <hx@aumc.cc>
2020-05-26 17:07:21 +0800

    js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox.jpeg') {
+        myImage.setAttribute('src', 'images/chrome.jpeg');
+    } else {
+        myImage.setAttribute('src', 'images/firefox.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello';
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    if (!myName || myName === null) {
+        setUserName();
+    }else{
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla ,' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mizilla ,' + storedName;
+}
+
+myButton.onclick = function () {
+    setUserName();
+}
